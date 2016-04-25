57023a4431f4cc2c8004eb0de4a8a887834a787b
StevenYu <stevenyuysy@gmail.com>
2016-04-25 15:43:00 +0800

    init

--- demo/stage_4/pxgallery.js
+++ demo/stage_4/pxgallery.js
@@ -1,12 +1,13 @@
 /**
-  *
-  * pxgallery v0.0.1
-  * Description, by StevenYu.
-  * @desc use with pxgallery.css
-  * @author StevenYu
-  */
+ *
+ * pxgallery v0.0.1
+ * Description, by StevenYu.
+ * @desc use with pxgallery.css
+ * @author StevenYu
+ */
 
 ;(function (root, factory) {
+  
   if (typeof define === 'function' && define.amd) {
     define([], factory);
   } else if (typeof module === 'object' && module.exports) {
@@ -15,56 +16,126 @@
     // Browser globals (root is window)
     root.pxgallery = factory();
   }
+  
 }(this, function() {
 
   'use strict';
 
   /**
-    * @param {Object} opts - options used in plugin
-    * @constructor
-    */
+   * @param {Object} opts - options used in plugin
+   * @constructor
+   */
 
   var pxgallery = function(opts) {
 
     
     opts = opts || {};
     var containerSelector = opts.containerSelector || '.pxgalleryContainer';
-    var boxSelector = opts.boxSelector || '.pxgalleryBox';
-    var boxes = document.querySelector(containerSelector).querySelectorAll(boxSelector);
-
+    
+    this.boxSelector = opts.boxSelector || '.pxgalleryBox';  
     this.container = document.querySelector(containerSelector);
         
-    this.LAYOUT = {
+    this.layout = {
       PUZZLE: 1,    // puzzle layout
       WATERFALL: 2, // waterfall layout
       BARREL: 3     // barrel layout
     };
-
-  
+ 
   };
 
   /**
-    * @desc Plugin prototype definition.
-    */
+   * @desc init the album
+   * It will replace the photos
+   * @param {(stirng | string[])} image - the URL of the photo or the URL array of the photos
+   * @param {object} opts - layout options(require)
+   */
 
-  pxgallery.prototype.setImage = function() {
+  pxgallery.prototype.setImage = function(image, opts) {
+    
+    if (typeof image === 'string') {
+      this.setImage([image]);
+      return;
+    }
+    
+    var layout = opts.layout; 
     
   };
+  
+  /**
+   * @desc get the DOM elements which contain the images
+   * @return {HTMLelement[]} boxes   
+   */
   
   pxgallery.prototype.getImageDomElements = function() {
-    
+    var boxes = this.container.querySelectorAll(this.boxSelector);
+    return boxes;
   };
   
-  pxgallery.prototype.addImage = function() {
-    
+  /**
+   * @desc add images to the container
+   * @param {(string | string[])} image - the URL of the photo or the URL array of the photos
+   */
+  
+  pxgallery.prototype.addImage = function(image) {
+    for (var i = 0; i < image.length; i++) {
+      var imageHtml = '<div class="' + layout.toLowerCase() + 'box"><img src="' + image[i] + '"></div>';
+      html += imageHtml;
+    }
+    this.container.innerHTML += html;
   };
   
-  pxgallery.prototype.removeImage = function() {
-    
+  /**
+   * @desc remove the images
+   * @param {(HTMLelement|HTMLelement[])} image - the images that need to be removed
+   */
+  
+  pxgallery.prototype.removeImage = function(image) {
+    var boxes = this.getImageDomElements();
+    boxes =  boxes.filter(function(item) {
+      return image.indexOf(item) === -1;
+    });
   };
   
-  pxgallery.prototype.setLayout = function() {
+  
+  
+  pxgallery.prototype.setLayout = function(layout) {
     
+  function setSquare () {
+    var boxes = this.getImageDomElements();
+    // if we need a square for the number 2 images when the length is 3 or 5, then we add a method for them
+    if (boxes.length === 3) {
+      this.setSquare = function() {
+        var sideLength = parseFloat(this.container.clientHeight) / 2;
+        boxes[0].style.width = (this.container.clientWidth - sideLength) + 'px';
+        boxes[1].style.height = sideLength + 'px';
+        boxes[1].style.width = sideLength + 'px';
+        boxes[2].style.height = sideLength + 'px';
+        boxes[2].style.width = sideLength + 'px';
+      };
+    } else if (boxes.length === 5) {
+      this.setSquare = function() {
+        var sideLength = parseFloat(this.container.clientWidth / 3);
+        boxes[0].style.width = parseFloat(this.container.clientWidth - sideLength) + 'px';
+        boxes[1].style.width = sideLength + 'px';
+        boxes[1].style.height = sideLength + 'px';
+        boxes[2].style.width = sideLength + 'px';
+        boxes[2].style.height = parseFloat(this.container.clientHeight - sideLength) + 'px';
+      };
+    }
+  }
+    
+    switch (layout) {
+    case 'PUZZLE':
+      this.container.className += 'puzzle-' + boxes.length;
+      setSquare();
+      break;
+    case 'WATERFALL':
+      this.container.className += 'waterfall';
+      break;
+    case 'BARREL':
+      this.container.className += 'barrel';
+      break;
+    }
   };
   
   pxgallery.prototype.setGutter = function() {
